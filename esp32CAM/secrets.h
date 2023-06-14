23a96f97bb2140d17bff5b7e36bdfdedcdff30ba
Rony Juliusburger <80697658+ronyju@users.noreply.github.com>
2023-06-14 20:22:34 +0300

    Update secrets.h

--- esp32CAM/secrets.h
+++ esp32CAM/secrets.h
@@ -1,1 +1,14 @@
 
+//Replace with your network credentials 
+const char* ssid = "OPPO A52"; 
+const char* password = "ronyrony";
+
+// Insert Firebase project API Key
+#define API_KEY "AIzaSyD6gDPaIt9KVwttNEB1rgQFIiYragbCxmo"
+
+// Insert Authorized Email and Corresponding Password
+#define USER_EMAIL "doorbelliot3@gmail.com"
+#define USER_PASSWORD "orenronyshachar"
+
+// Insert Firebase storage bucket ID e.g bucket-name.appspot.com
+#define STORAGE_BUCKET_ID "doorbell2-d3381.appspot.com"
